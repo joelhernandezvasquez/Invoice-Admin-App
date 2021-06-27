30f2e59332e1afc1f85d717b0551db8d4a345e44
joelhernandezvasquez <jhernandez98@outlook.es>
2021-06-27 08:58:23 -0400

    set main colors, set main fonts, install fonts,create partials

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../sass/main.scss';
 
 const App = () => {
     return (
-        <div>
-            hello app
-        </div>
+        <>
+        
+        </>
     )
 }
 
