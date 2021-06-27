439de37961da1be35a77844c3f3c63a8c718e50f
joelhernandezvasquez <jhernandez98@outlook.es>
2021-06-27 10:17:12 -0400

    added home component, add home sccss layout page and added an utility partial file

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Home = () => {
     return (
         <section className="home-page-wrapper">
-            
+            <div className="graphic-bg">
+                <div className="upper-line line"></div>
+                
+            </div>
         </section>
     )
 }
