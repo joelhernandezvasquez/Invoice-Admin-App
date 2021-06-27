1b6ad8b421fccb0707fb89511e7266d10fafbcc3
joelhernandezvasquez <jhernandez98@outlook.es>
2021-06-27 09:04:46 -0400

    added home page component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Home from './Home';
 import '../sass/main.scss';
 
 const App = () => {
     return (
         <>
-        
+        <Home/>
         </>
     )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section className="home-page-wrapper">
+            
+        </section>
+    )
+}
+
+export default Home;
