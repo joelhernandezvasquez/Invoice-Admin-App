a5df7795b1033266183ee1d517431e07d7931665
joelhernandezvasquez <jhernandez98@outlook.es>
2021-06-28 14:03:52 -0400

    added hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import NavBar from './NavBar';
+import HeroImage from '../assets/images/hero.svg';
+import BG from '../assets/images/Bg.svg';
 
 const Home = () => {
     return (
         <section className="home-page-wrapper">
-            <div className="graphic-bg">
-                <div className="upper-line line"></div>
-                <div className="upper-line line"></div> 
-            </div>
-            <NavBar/>
+                <div className="graphic-bg">
+                    <div className="upper-line line"></div>
+                    <div className="upper-line line"></div> 
+                </div>
+                <NavBar/>
+                
+                 <div className="hero-section">
+                  
+                     <div className="container">
+                        <div className="hero-img-container">
+                         <img className="hero-img" src={HeroImage} alt="hero"/>
+                        </div>
+                      
+                      <div className="hero-content">
+                          <h1>Make Your Bussiness Easier & <span>Simple</span></h1>
+                          <p>The poster concept but the never back we to and desk noise towards never of thought, of merit business least exerted considerable.</p>
+                      </div>
+                   </div> 
+
+                   <img className="bg-hero-shape" src={BG} alt="shape background" />
+                    <div className="bottom-line line"></div>
+                </div> 
+               
            
         </section>
     )
