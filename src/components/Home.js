e0d64a9ddd7e158c8c82a5e3fc7bf9412e6c97d1
joelhernandezvasquez <jhernandez98@outlook.es>
2021-06-29 08:47:57 -0400

    added hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 import HeroImage from '../assets/images/hero.svg';
 import BG from '../assets/images/Bg.svg';
 
+
 const Home = () => {
     return (
         <section className="home-page-wrapper">
@@ -20,9 +21,11 @@ const Home = () => {
                         </div>
                       
                       <div className="hero-content">
-                          <h1>Make Your Bussiness Easier & <span>Simple</span></h1>
-                          <p>The poster concept but the never back we to and desk noise towards never of thought, of merit business least exerted considerable.</p>
+                          <h1>Make Your Business Easier & <span> Simple</span></h1>
+                          <p className="leading-text text-center text">The poster concept but the never back we to and desk noise towards never of thought, of merit business least exerted considerable.</p>
+                          <button className="btn btn-primary"> Try Free Trial </button> 
                       </div>
+
                    </div> 
 
                    <img className="bg-hero-shape" src={BG} alt="shape background" />
