d923c310b0d3eb63666530bd7462b15a5faea99a
joelhernandezvasquez <jhernandez98@outlook.es>
2021-07-03 16:09:50 -0400

    added and complete mobile version of feature section

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import FeatureCard from './FeatureCard';
+import  timeTrackingIcon from '../assets/images/timeTrackingIcon.svg';
+import  monthlyStatsIcon from '../assets/images/monthlyStatsIcon.svg';
+import  settingsIcon from '../assets/images/settingsIcon.svg';
 
 const Features = () => {
     return (
@@ -8,7 +12,27 @@ const Features = () => {
                <p className="features-subheading leading-text text-center">Our powerfull AI tool organizes sales reports for daily, weekly, monthly and overall yearly. Which leads less human works and maintenence</p>
             
                <div className="features-card-container">
-                   
+                   <FeatureCard
+                     thumbnailIcon = {timeTrackingIcon}
+                     title = "Time Tracking"
+                     content = "Have the sacred, reached could livethought this is to the easy apparent all tonesHave the sacred, reached could."
+                     link = "#"
+                   />
+
+                   <FeatureCard
+                    thumbnailIcon = {monthlyStatsIcon}
+                    title = "Monthly Stats"
+                    content = "Have the sacred, reached could livethought this is to the easy apparent all tonesHave the sacred, reached could."
+                    link = "#"
+                   />
+
+                   <FeatureCard
+                    thumbnailIcon = {settingsIcon}
+                    title = "Online Customize"
+                    content = "Have the sacred, reached could livethought this is to the easy apparent all tonesHave the sacred, reached could."
+                    link = "#"
+                   />
+                  
                </div>
             </div>
         </section>
