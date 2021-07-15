e6d77ff67de19618ff2552bff874e987984bdd5f
joelhernandezvasquez <jhernandez98@outlook.es>
2021-07-15 11:18:56 -0400

    added formik to handle forms

--- src/components/Forms/AuthForm.js
+++ src/components/Forms/AuthForm.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import {useFormik} from 'formik';
 import LogoIcon from '../../assets/images/logo.svg';
 import GoogleIcon from '../../assets/images/googleIcon.svg';
 
-const AuthForm = ({Title,Access}) => {
+const AuthForm = ({Title,Access,Type,handleLogin}) => {
+    
+    const formik = useFormik({
+        initialValues:{
+            email:'',
+            password:''
+        },
+        onSubmit: values =>{
+            console.log('form values:',values);
+        },
+        validate: values =>{
+            
+        }
+
+       
+    })
+
+   
+    
+    const renderAccessText = ()=>{
+
+        if(Type == 'login' || Type ==='Login')
+        {
+        return(
+          <p className="account-access-text"> Not registered yet? <a href="#">Create an Account?</a></p>
+        )
+        }
+
+        if(Type =='sign in' || 'Sign in')
+        {
+         return(
+            <p className ="account-access-text">Already have an Account? <a href="#">Sign in?</a></p> 
+         )
+        }
+    }
     return (
-        <form className="auth-form">
+        
+        <form className="auth-form" onSubmit={formik.handleSubmit}>
            <img className="logo" src={LogoIcon} alt ="logo icon"/>
            <h2 className="section-title text-center">{Title}</h2>
            <button className="google-btn">
@@ -16,6 +52,28 @@ const AuthForm = ({Title,Access}) => {
                <span>Or</span>
                <div className="line"></div>
            </div>
+
+           <div className="form-control">
+               <label className="primary-label" htmlFor="email">email</label>
+               <input className="text-field" 
+               type="email" 
+               name ="email" 
+               onChange = {formik.handleChange}
+               value = {formik.values.email}/>
+           </div>
+
+           <div className="form-control">
+               <label className="primary-label" htmlFor="password">password</label>
+               <input className="text-field" 
+               type="password" 
+               name ="password" 
+               onChange = {formik.handleChange}
+               value = {formik.values.password}/>
+           </div>
+           
+           <button  className="btn btn-primary auth-btn" type="submit" onClick = {()=> handleLogin()}>{Type}</button>
+
+           {renderAccessText()}
         </form>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import AuthForm from './Forms/AuthForm';
 import GraphicBg from '../assets/images/authGraphic.png';
 import Frame from '../assets/images/Frame.png';
 
 const Login = () => {
+    
+
+    const handleLogin = () =>{
+        console.log("login")
+    }
     return (
+        
         <div className="auth-container">
             
             <div className="graphic">
@@ -19,10 +25,14 @@ const Login = () => {
                 <AuthForm
                  Title = "Log Into Your Invoicely Account"
                  Access = "Sign in with Google"
-                />
-               
-                
+                 Type="login"
+                 handleLogin = {handleLogin}
+                />     
             </div>
+
+            <footer className="container">
+                <p>Copyright © 2021 OX . All rights reserved.</p>
+            </footer>
         </div>
     )
 }
