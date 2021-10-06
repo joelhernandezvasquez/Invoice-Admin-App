e4cd953ddde08b892aa5bfb79b059a017d734a2c
joelhernandezvasquez <jhernandez98@outlook.es>
2021-10-06 09:12:15 -0400

    prototype customer page

--- src/components/Customers/CustomerForm.js
+++ src/components/Customers/CustomerForm.js
@@ -5,19 +5,19 @@ const CustomerForm = () => {
         <div className="customer-form">
           <h4 className="sub-heading"> Basic Info</h4> 
            <form className="main-form">
-               <div className="form-input-container">
+               <div className="form-input-container name">
                   <label for="">customer's name</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                </div>
 
-               <div className="form-input-container">
+               <div className="form-input-container email">
                   <label for="">customer's email</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                </div>
 
-               <div className="form-input-container">
+               <div className="form-input-container phone-number">
                   <label for="">phone number</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                </div>
 
                <h4 className="sub-heading"> Billing Address</h4> 
@@ -25,29 +25,29 @@ const CustomerForm = () => {
                 
                 <div className="form-input-container street-address">
                   <label for="">street address</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                 </div>
 
                 <div className="form-input-container city">
                   <label for="">city</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                 </div>
 
                 <div className="form-input-container post-code">
                   <label for="">post code</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                 </div>
 
                 
                 <div className="form-input-container country">
                   <label for="">country</label>
-                  <input type ="text"/> 
+                  <input type ="text" className="input-text"/> 
                 </div>
 
 
                 </div>
 
-                <button>Add Customer</button>
+                <button className="submit-btn">Add Customer</button>
            </form>
         </div>
     )
